7404781ff4d537ab5c1b7785d1d7620725e4af0d
Marta Hilgert <marta.hil.1988@gmail.com>
2024-09-10 21:03:11 -0300

    Goal "No smartphone use at night" - created

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,19 +1,26 @@
 import fastify from 'fastify'
+import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod"
 import { createGoal } from '../functions/create-goal'
 import z from 'zod'
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>( )
 
-app.post('/goals', async (request) => {
-  const createGoalSchema = z.object({
-    title: z.string(),
-    desiredWeeklyFrequency: z.number().int().min(1).max(7),
-  })
-  const body = createGoalSchema.parse(request.body)
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.post('/goals', {
+  schema: {
+    body: z.object({
+      title: z.string(),
+      desiredWeeklyFrequency: z.number().int().min(1).max(7),
+    }),
+  }
+}, async (request) => {
+  const { title, desiredWeeklyFrequency } = request.body
 
   await createGoal({
-    title: body.title,
-    desiredWeeklyFrequency: body.desiredWeeklyFrequency,
+    title,
+    desiredWeeklyFrequency,
   })
 
 })
