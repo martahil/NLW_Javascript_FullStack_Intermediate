ca8999ccdb56dd03bef416c6332f2b9538e29505
Marta Hilgert <marta.hil.1988@gmail.com>
2024-09-11 23:14:23 -0300

    app.register - fastifyCors

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,12 +1,16 @@
 import fastify from 'fastify'
 import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod"
-
 import { createGoalRoute } from './routes/create-goal'
 import { createCompletionRoute } from './routes/create-completion'
 import { getPendingGoalsRoute } from './routes/get-pending-goals'
 import { getWeekSummaryRoute } from './routes/get-week-summary'
+import fastifyCors from '@fastify/cors'
 
-const app = fastify().withTypeProvider<ZodTypeProvider>( )
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: '*',
+})
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
