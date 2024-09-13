d641c6db751d0f2e3faca429d309be7387985e2b
Marta Hilgert <marta.hil.1988@gmail.com>
2024-09-13 12:49:31 -0300

    components/ui - included

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -2,6 +2,7 @@ import { Plus } from 'lucide-react'
 
 import logo from './assets/logo-in-orbit.svg'
 import letsStart from './assets/lets-start-illustration.svg'
+import { Button } from './components/ui/button'
 
 export function App() {
   return (
@@ -12,13 +13,18 @@ export function App() {
         You haven't set any goals yet. How about setting one right now?
       </p>
 
-      <button
+      <Button>
+        <Plus className="size-4" />
+        Create a goal
+      </Button>
+
+      {/* <button
         type="button"
         className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 text-sm font-medium tracking-tight hover:bg-violet-600"
       >
         <Plus className="size-4" />
         Create a goal
-      </button>
+      </button> */}
     </div>
   )
 }
