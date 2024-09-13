f19411949f8626b2ea2fd822c8d5a78a3e4a1a2e
Marta Hilgert <marta.hil.1988@gmail.com>
2024-09-12 23:48:36 -0300

    Create a goal page

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,3 +1,24 @@
+import { Plus } from 'lucide-react'
+
+import logo from './assets/logo-in-orbit.svg'
+import letsStart from './assets/lets-start-illustration.svg'
+
 export function App() {
-  return <h1 className="bg-black text-white text-3xl">Hello</h1>
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="in-orbit" />
+      <img src={letsStart} alt="in-orbit" />
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        You haven't set any goals yet. How about setting one right now?
+      </p>
+
+      <button
+        type="button"
+        className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 text-sm font-medium tracking-tight hover:bg-violet-600"
+      >
+        <Plus className="size-4" />
+        Create a goal
+      </button>
+    </div>
+  )
 }
