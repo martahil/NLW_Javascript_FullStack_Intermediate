ceb597dc8fddb81d61831a916107858c1b81d741
Marta Hilgert <marta.hil.1988@gmail.com>
2024-09-14 23:39:13 -0300

    Info displayed - hour

--- web/src/components/summary.tsx
+++ web/src/components/summary.tsx
@@ -101,14 +101,20 @@ export function Summary() {
               </h3>
 
               <ul className="flex flex-col gap-3">
-                <li className="flex items-center gap-2">
-                  <CheckCircle2 className="size-4 text-pink-500" />
-                  <span className="text-sm text-zinc-400">
-                    You completed "
-                    <span className="text-zinc-100">Wake up early</span>" at{' '}
-                    <span className="text-zinc-100">8:13 AM</span>
-                  </span>
-                </li>
+                {goals.map(goal => {
+                  const time = dayjs(goal.completedAt).format('HH:mm A')
+
+                  return (
+                    <li key={goal.id} className="flex items-center gap-2">
+                      <CheckCircle2 className="size-4 text-pink-500" />
+                      <span className="text-sm text-zinc-400">
+                        You completed "
+                        <span className="text-zinc-100">{goal.title}</span>" at{' '}
+                        <span className="text-zinc-100">{time}</span>
+                      </span>
+                    </li>
+                  )
+                })}
               </ul>
             </div>
           )
